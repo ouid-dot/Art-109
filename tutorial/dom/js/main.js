b03d79f41c2dd8f196ad492dbab4fa87a202416c
ouid-dot <ruizjake3146@gmail.com>
2025-02-18 17:39:15 -0800

    dom tut

--- tutorial/dom/js/main.js
+++ tutorial/dom/js/main.js
@@ -1,5 +1,65 @@
 
-console.log("hello hello");
+let divs = document.querySelectorAll(".image-div");
 
-document.querySelector("#page-title").style.color = "pink";
+divs.forEach((div) => {
+    div.addEventListener('click',() => {
+        console.log("forEach worked");
+        div.style.visibility = "hidden";
+    })
+})
+
+
+/*document.querySelector("#tiny").addEventListener("click", function(){
+    document.querySelector("#tiny").style.display = "none";
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+console.log("hello hello");
+// javascript timeout timer changes title color
+
+let pageTitle = document.querySelector("#page-title");
+let header = document.querySelector("header");
+
+
+setTimeout(function(){
+    pageTitle.style.color = "pink";
+    //console.log("timeout worked!")
+} ,3000 );
+
+//click event on header changes background color
+header.onclick = function(){
+    //console.log("clicked header")
+    document.querySelector("body").style.backgroundColor = "black"
+}
+ */
+
+
+
+
+
 
